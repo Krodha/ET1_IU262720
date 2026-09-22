fad76cb8ac391fa9208043b021a4f389952ddc5d
Lucas Cruces <87600120+Krodha@users.noreply.github.com>
2026-09-22 20:15:57 +0200

    Commit inicial
    Creación del fichero para indicar los datos de la entrega en una variable de JavaScript. FICHERO SIN DATOS CUBIERTOS

---
+++ ET1_IU262720.js
@@ -0,0 +1,11 @@
+let datosgenerales =
+{
+    Lider:
+        ["CrucesFernandezLucas", "Entrega", 0],
+    Participante1:
+        ["LimaGandaraMartin", "Entrega", 0],
+    Participante2:
+        ["DizParrondoRoberto", "Entrega", 0],
+    Participante3:
+        ["ElsdekyMahmoudKhaled", "Entrega", 0]
+}
