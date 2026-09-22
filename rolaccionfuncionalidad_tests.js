4b623d95d1394738702d100804616a6b22b321e8
Lucas Cruces <87600120+Krodha@users.noreply.github.com>
2026-09-22 20:47:12 +0200

    Insercion de plantillas de tests
    Insercion de plantillas de tests para cada una de las entidades de la base de datos. Cada fichero de *_tests.js contiene dos variables: una para la definicion de los tests y otra para la definicion de las pruebas que usarán los tests.

---
+++ rolaccionfuncionalidad_tests.js
@@ -0,0 +1,16 @@
+let rolaccionfuncionalidad_def_tests = 
+//Array de arrays
+Array(
+    //Esquema de arrays para definicion de tests...
+    Array('entidad','campo_entidad','elemento_formulario',1,'descripcion_test','validacion_atomica','accion','nombre_error_ko','Descripcion_del_fallo_y_como_solucionarlo'),
+    Array('entidad','campo_entidad','elemento_formulario',2,'descripcion_test','validacion_atomica','accion','nombre_error_ko','Descripcion_del_fallo_y_como_solucionarlo'),
+
+);
+
+//Definicion de pruebas que usen los tests creados anteriormente
+let rolaccionfuncionalidad_pruebas = 
+Array(
+    //Esquema de arrays para la definicion de pruebas...
+    Array('entidad','campo_entidad',1,1,'accion',{atributo:'valores_a_probar_en_atributo'},'resultado_esperado_de_la_prueba'),
+    Array('entidad','campo_entidad',2,2,'accion',{atributo:'valores_a_probar_en_atributo'},'resultado_esperado_de_la_prueba'),
+);
