f05e26a0636fd9057072326b1ad7d776cfa4822f
Lucas Cruces <87600120+Krodha@users.noreply.github.com>
2026-09-26 17:16:35 +0200

    rolaccionfuncionalidad_tests.js completado
    rolaccionfuncionalidad_tests.js v1

--- rolaccionfuncionalidad_tests.js
+++ rolaccionfuncionalidad_tests.js
@@ -1,16 +1,107 @@
 let rolaccionfuncionalidad_def_tests = 
 //Array de arrays
 Array(
-    //Esquema de arrays para definicion de tests...
-    Array('entidad','campo_entidad','elemento_formulario',1,'descripcion_test','validacion_atomica','accion','nombre_error_ko','Descripcion_del_fallo_y_como_solucionarlo'),
-    Array('entidad','campo_entidad','elemento_formulario',2,'descripcion_test','validacion_atomica','accion','nombre_error_ko','Descripcion_del_fallo_y_como_solucionarlo'),
+    //id_funcionalidad --> ADD
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',1,'cumple tamaño minimo','min_size','ADD','id_funcionalidad_rolaccionfuncionalidad_min_size_ko','Identificador de funcionalidad demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',2,'cumple tamaño máximo','max_size','ADD','id_funcionalidad_rolaccionfuncionalidad_max_size_ko','Identificador de funcionalidad demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',3,'cumple formato','format','ADD','id_funcionalidad_rolaccionfuncionalidad_format_ko','Identificador de funcionalidad no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',4,'identificador correcto','valid','ADD',true,'Identificador de funcionalidad correcto'),
+    //id_funcionalidad --> EDIT
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',5,'cumple tamaño minimo','min_size','EDIT','id_funcionalidad_rolaccionfuncionalidad_min_size_ko','Identificador de funcionalidad demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',6,'cumple tamaño máximo','max_size','EDIT','id_funcionalidad_rolaccionfuncionalidad_max_size_ko','Identificador de funcionalidad demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',7,'cumple formato','format','EDIT','EDIT','id_funcionalidad_rolaccionfuncionalidad_format_ko','Identificador de funcionalidad no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',8,'identificador correcto','valid','EDIT',true,'Identificador de funcionalidad correcto'),
+    //id_funcionalidad --> SEARCH
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',9,'cumple tamaño minimo','min_size','SEARCH','id_funcionalidad_rolaccionfuncionalidad_min_size_ko','Identificador de funcionalidad demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',10,'cumple tamaño máximo','max_size','SEARCH','id_funcionalidad_rolaccionfuncionalidad_max_size_ko','Identificador de funcionalidad demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',11,'cumple formato','format','SEARCH','id_funcionalidad_rolaccionfuncionalidad_format_ko','Identificador de funcionalidad no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad','input',12,'identificador correcto','valid','SEARCH',true,'Identificador de funcionalidad correcto'),
+    
 
+    //id_accion --> ADD
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',13,'cumple tamaño minimo','min_size','ADD','id_accion_rolaccionfuncionalidad_min_size_ko','Identificador de accion demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',14,'cumple tamaño máximo','max_size','ADD','id_accion_rolaccionfuncionalidad_max_size_ko','Identificador de accion demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',15,'cumple formato','format','ADD','id_accion_rolaccionfuncionalidad_format_ko','Identificador de accion no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',16,'identificador correcto','valid','ADD',true,'Identificador de accion correcto'),
+    //id_accion --> EDIT
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',17,'cumple tamaño minimo','min_size','EDIT','id_accion_rolaccionfuncionalidad_min_size_ko','Identificador de accion demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',18,'cumple tamaño máximo','max_size','EDIT','id_accion_rolaccionfuncionalidad_max_size_ko','Identificador de accion demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',19,'cumple formato','format','EDIT','id_accion_rolaccionfuncionalidad_format_ko','Identificador de accion no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',20,'identificador correcto','valid','EDIT',true,'Identificador de accion correcto'),
+    //id_accion --> SEARCH
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',21,'cumple tamaño minimo','min_size','SEARCH','id_accion_rolaccionfuncionalidad_min_size_ko','Identificador de accion demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',22,'cumple tamaño máximo','max_size','SEARCH','id_accion_rolaccionfuncionalidad_max_size_ko','Identificador de accion demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',23,'cumple formato','format','SEARCH','id_accion_rolaccionfuncionalidad_format_ko','Identificador de accion no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad','input',24,'identificador correcto','valid','SEARCH',true,'Identificador de accion correcto'),
+
+
+    //id_rol --> ADD
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',25,'cumple tamaño minimo','min_size','ADD','id_rol_rolaccionfuncionalidad_min_size_ko','Identificador de rol demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',26,'cumple tamaño máximo','max_size','ADD','id_rol_rolaccionfuncionalidad_max_size_ko','Identificador de rol demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',27,'cumple formato','format','ADD','id_rol_rolaccionfuncionalidad_format_ko','Identificador de rol no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',28,'identificador correcto','valid','ADD',true,'Identificador de rol correcto'),
+    //id_rol --> EDIT
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',29,'cumple tamaño minimo','min_size','EDIT','id_rol_rolaccionfuncionalidad_min_size_ko','Identificador de rol demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',30,'cumple tamaño máximo','max_size','EDIT','id_rol_rolaccionfuncionalidad_max_size_ko','Identificador de rol demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',31,'cumple formato','format','EDIT','id_rol_rolaccionfuncionalidad_format_ko','Identificador de rol no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',32,'identificador correcto','valid','EDIT',true,'Identificador de rol correcto'),
+    //id_rol --> SEARCH
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',33,'cumple tamaño minimo','min_size','SEARCH','id_rol_rolaccionfuncionalidad_min_size_ko','Identificador de rol demasiado corto. Debe tener al menos un dígito'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',34,'cumple tamaño máximo','max_size','SEARCH','id_rol_rolaccionfuncionalidad_max_size_ko','Identificador de rol demasiado corto. Debe tener 11 dígitos como máximo'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',35,'cumple formato','format','SEARCH','id_rol_rolaccionfuncionalidad_format_ko','Identificador de rol no cumple formato. Solo se permiten números'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad','input',36,'identificador correcto','valid','SEARCH',true,'Identificador de rol correcto'),
 );
 
 //Definicion de pruebas que usen los tests creados anteriormente
 let rolaccionfuncionalidad_pruebas = 
 Array(
-    //Esquema de arrays para la definicion de pruebas...
-    Array('entidad','campo_entidad',1,1,'accion',{atributo:'valores_a_probar_en_atributo'},'resultado_esperado_de_la_prueba'),
-    Array('entidad','campo_entidad',2,2,'accion',{atributo:'valores_a_probar_en_atributo'},'resultado_esperado_de_la_prueba'),
+    //id_funcionalidad --> ADD
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',1,1,'ADD',{id_funcionalidad_rolaccionfuncionalidad:''},'id_funcionalidad_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',2,2,'ADD',{id_funcionalidad_rolaccionfuncionalidad:1234567891011},'id_funcionalidad_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',3,3,'ADD',{id_funcionalidad_rolaccionfuncionalidad:'A3j'},'id_funcionalidad_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',4,4,'ADD',{id_funcionalidad_rolaccionfuncionalidad:123456},true),
+    //id_funcionalidad --> EDIT
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',5,5,'EDIT',{id_funcionalidad_rolaccionfuncionalidad:''},'id_funcionalidad_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',6,6,'EDIT',{id_funcionalidad_rolaccionfuncionalidad:1234567891011},'id_funcionalidad_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',7,7,'EDIT',{id_funcionalidad_rolaccionfuncionalidad:'A3j'},'id_funcionalidad_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',8,8,'EDIT',{id_funcionalidad_rolaccionfuncionalidad:123456},true),
+    //id_funcionalidad --> SEARCH
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',9,9,'SEARCH',{id_funcionalidad_rolaccionfuncionalidad:''},'id_funcionalidad_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',10,10,'SEARCH',{id_funcionalidad_rolaccionfuncionalidad:1234567891011},'id_funcionalidad_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',11,11,'SEARCH',{id_funcionalidad_rolaccionfuncionalidad:'A3j'},'id_funcionalidad_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_funcionalidad_rolaccionfuncionalidad',12,12,'SEARCH',{id_funcionalidad_rolaccionfuncionalidad:123456},true),
+
+
+    //id_accion --> ADD
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',13,13,'ADD',{id_accion_rolaccionfuncionalidad:''},'id_accion_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',14,14,'ADD',{id_accion_rolaccionfuncionalidad:1234567891011},'id_accion_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',15,15,'ADD',{id_accion_rolaccionfuncionalidad:'A3j'},'id_accion_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',16,16,'ADD',{id_accion_rolaccionfuncionalidad:123456},true),
+    //id_accion --> EDIT
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',17,17,'EDIT',{id_accion_rolaccionfuncionalidad:''},'id_accion_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',18,18,'EDIT',{id_accion_rolaccionfuncionalidad:1234567891011},'id_accion_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',19,19,'EDIT',{id_accion_rolaccionfuncionalidad:'A3j'},'id_accion_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',20,20,'EDIT',{id_accion_rolaccionfuncionalidad:123456},true),
+    //id_accion --> SEARCH
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',21,21,'SEARCH',{id_accion_rolaccionfuncionalidad:''},'id_accion_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',22,22,'SEARCH',{id_accion_rolaccionfuncionalidad:1234567891011},'id_accion_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',23,23,'SEARCH',{id_accion_rolaccionfuncionalidad:'A3j'},'id_accion_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_accion_rolaccionfuncionalidad',24,24,'SEARCH',{id_accion_rolaccionfuncionalidad:123456},true),
+
+
+    //id_rol --> ADD
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',25,25,'ADD',{id_rol_rolaccionfuncionalidad:''},'id_rol_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',26,26,'ADD',{id_rol_rolaccionfuncionalidad:1234567891011},'id_rol_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',27,27,'ADD',{id_rol_rolaccionfuncionalidad:'A3j'},'id_rol_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',28,28,'ADD',{id_rol_rolaccionfuncionalidad:123456},true),
+    //id_rol --> EDIT
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',29,29,'EDIT',{id_rol_rolaccionfuncionalidad:''},'id_rol_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',30,30,'EDIT',{id_rol_rolaccionfuncionalidad:1234567891011},'id_rol_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',31,31,'EDIT',{id_rol_rolaccionfuncionalidad:'A3j'},'id_rol_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',32,32,'EDIT',{id_rol_rolaccionfuncionalidad:123456},true),
+    //id_rol --> SEARCH
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',33,33,'SEARCH',{id_rol_rolaccionfuncionalidad:''},'id_rol_rolaccionfuncionalidad_min_size_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',34,34,'SEARCH',{id_rol_rolaccionfuncionalidad:1234567891011},'id_rol_rolaccionfuncionalidad_max_size_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',35,35,'SEARCH',{id_rol_rolaccionfuncionalidad:'A3j'},'id_rol_rolaccionfuncionalidad_format_ko'),
+    Array('rolaccionfuncionalidad','id_rol_rolaccionfuncionalidad',36,36,'SEARCH',{id_rol_rolaccionfuncionalidad:123456},true),
 );
